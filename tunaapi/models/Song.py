d46d14b61880c564ce43dfe30fe3adca60d7a0c8
Christy Le <lechrsty@gmail.com>
2023-02-21 23:10:56 -0800

    query songs by artist_id or genre_id

--- tunaapi/models/Song.py
+++ tunaapi/models/Song.py
@@ -5,5 +5,5 @@ class Song(models.Model):
     title = models.CharField(max_length=55)
     album = models.CharField(max_length=500)
     length = models.IntegerField()
-    genre = models.ForeignKey("Genre", on_delete=models.SET_NULL, null=True)
-    artist = models.ForeignKey("Artist", on_delete=models.CASCADE, related_name="SongArtist")
+    genre = models.ForeignKey("Genre", on_delete=models.SET_NULL, null=True, related_name='genre')
+    artist = models.ForeignKey("Artist", on_delete=models.CASCADE, related_name="artist")
