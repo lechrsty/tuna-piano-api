e0d51d3d5c348ef4a8962b3d37f325a63abab874
Christy Le <lechrsty@gmail.com>
2023-02-21 23:47:57 -0800

    created custom decorator that retrieves Song objects associated with Artists and adds them as a new key-value pair to the serialized data returned by the view

--- tunaapi/models/Artist.py
+++ tunaapi/models/Artist.py
@@ -1,8 +1,7 @@
 from django.db import models
+from .Song import Song
 
 class Artist(models.Model):
-    
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     bio = models.CharField(max_length=50)
-    # songs = models.ForeignKey("Song", on_delete=models.SET_NULL, null=True, related_name="songs")
--- tunaapi/views/artist.py
+++ tunaapi/views/artist.py
@@ -1,51 +1,61 @@
-"""View module for handling requests about artist types"""
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import Song, Genre, Artist
+from tunaapi.models import Song, Artist
 
+def join_songs(view_func):
+    def wrapper(*args, **kwargs):
+        # Call the actual view function
+        response = view_func(*args, **kwargs)
+
+        # Add songs to artist data
+        if isinstance(response.data, list):
+            for artist_data in response.data:
+                artist_id = artist_data['id']
+                songs = Song.objects.filter(artist_id=artist_id)
+                songs_data = SongSerializer(songs, many=True).data
+                artist_data['songs'] = songs_data
+        else:
+            artist_id = response.data['id']
+            songs = Song.objects.filter(artist_id=artist_id)
+            songs_data = SongSerializer(songs, many=True).data
+            response.data['songs'] = songs_data
+
+        return response
+    return wrapper
 
 class ArtistView(ViewSet):
 
+    @join_songs
     def retrieve(self, request, pk):
-
         artist = Artist.objects.get(pk=pk)
         serializer = ArtistSerializer(artist)
         return Response(serializer.data)
 
-
+    @join_songs
     def list(self, request):
-
         artists = Artist.objects.all()
         serializer = ArtistSerializer(artists, many=True)
         return Response(serializer.data)
 
+    @join_songs
     def create(self, request):
-
-        # songs = Song.objects.get(pk=request.data["songs"])
-
         artist = Artist.objects.create(
             name=request.data["name"],
             age=request.data["age"],
             bio=request.data["bio"]
-            # songs = songs
         )
 
         serializer = ArtistSerializer(artist)
         return Response(serializer.data)
 
-# class SongsArtistSerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Song
-#         fields = ('id', 'title', 'album' )
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ('id', 'title', 'album')
 
 class ArtistSerializer(serializers.ModelSerializer):
-
-    # songs = SongsArtistSerializer(many=False)
-
     class Meta:
         model = Artist
         fields = ('id', 'name', 'age', 'bio')
-        depth = 1
